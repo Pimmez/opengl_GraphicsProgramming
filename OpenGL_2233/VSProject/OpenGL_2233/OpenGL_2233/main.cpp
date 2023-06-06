b0390a9d78191dcebc41e48f2f9263f8dd21b9f4
Pim Horeman <pimhoreman@gmail.com>
2023-06-06 15:32:19 +0200

    Time To Triangle: Black Triangle Draw working

--- OpenGL_2233/VSProject/OpenGL_2233/OpenGL_2233/main.cpp
+++ OpenGL_2233/VSProject/OpenGL_2233/OpenGL_2233/main.cpp
@@ -4,9 +4,58 @@
 
 //Forward Declaration
 void processInput(GLFWwindow* window);
+int init(GLFWwindow*& window);
+void createTriangle(GLuint &vao, int &size);
+void createShaders();
+
 
 int main() {
 
+	GLFWwindow* window;
+	int res = init(window);
+	if (res != 0)
+	{
+		return res;
+	}
+
+	GLuint triangleVAO;
+	int triangleSize;
+	createTriangle(triangleVAO, triangleSize);
+	createShaders();
+
+	//Tell opengl to create viewport
+	glViewport(0, 0, 1280, 720);
+	
+	//Rendering loop
+	while (!glfwWindowShouldClose(window))
+	{
+		//Input
+		processInput(window);
+
+		//Rendering
+		glClearColor(0.5f, 0.2f, 0.9f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(triangleVAO);
+		glDrawArrays(GL_TRIANGLES, 0, triangleSize);
+
+		//Swap & Poll	
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+int init(GLFWwindow*& window)
+{
 	//GLFW Init
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -15,7 +64,7 @@ int main() {
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	//Create window & make active context	
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "OpenGL_2233", NULL, NULL);
+	window = glfwCreateWindow(1280, 720, "OpenGL_2233", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -31,30 +80,36 @@ int main() {
 		return -1;
 	}
 
-	//Tell opengl to create viewport
-	glViewport(0, 0, 1280, 720);
-	
-	//Rendering loop
-	while (!glfwWindowShouldClose(window))
-	{
-		//Input
-		processInput(window);
-
-		//Rendering
-		glClearColor(0.5f, 0.2f, 0.9f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		//Swap & Poll	
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
 	return 0;
 }
 
-void processInput(GLFWwindow* window)
+void createTriangle(GLuint& vao, int& size)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
+
+	//VertexArrayObject
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	//VertexBufferObject
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	size = sizeof(vertices);
+
+}
+
+void createShaders()
+{
+
 }
