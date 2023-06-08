6b99f00a8fa60049f6bff487973faf91ca0b1d33
Pim Horeman <pimhoreman@gmail.com>
2023-06-08 20:09:57 +0200

    Time To Triangle + Drawing and Rendering a square works.
    Had an error with the loadfile not reading the file. Apparently std::Fstream is not using the flag set for std::ios::in
    Fixed the error with Aaron and continued working on shading custom geometry

--- OpenGL_2233/VSProject/OpenGL_2233/OpenGL_2233/main.cpp
+++ OpenGL_2233/VSProject/OpenGL_2233/OpenGL_2233/main.cpp
@@ -1,39 +1,44 @@
 #include <iostream>
+#include <fstream>
+
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 //Forward Declaration
 void processInput(GLFWwindow* window);
+int init(GLFWwindow*& window);
+void createTriangle(GLuint& vao, GLuint &ebo, int& size, int& numIndices);
+void createShaders();
+void createProgram(GLuint& programID, const char* vertex, const char* fragment);
+
+//Utilities
+void loadFile(const char* filename, char*& output);
+
+//Program IDs
+GLuint simpleProgram;
 
 int main() {
 
-	//GLFW Init
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-	//Create window & make active context	
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "OpenGL_2233", NULL, NULL);
-	if (window == NULL)
+	//Init
+	GLFWwindow* window;
+	int result = init(window);
+	if (result != 0)
 	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
+		return result;
 	}
-	glfwMakeContextCurrent(window);
+	
+	createShaders();
 
-	//Load GLAD 
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
+	GLuint triangleVAO, triangleEBO;
+	int triangleSize, triangleIndexCount;
+	createTriangle(triangleVAO, triangleEBO, triangleSize, triangleIndexCount);
 
 	//Tell opengl to create viewport
 	glViewport(0, 0, 1280, 720);
-	
+
 	//Rendering loop
 	while (!glfwWindowShouldClose(window))
 	{
@@ -43,6 +48,12 @@ int main() {
 		//Rendering
 		glClearColor(0.5f, 0.2f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(simpleProgram);
+
+		glBindVertexArray(triangleVAO);
+		//glDrawArrays(GL_TRIANGLES, 0, triangleSize);
+		glDrawElements(GL_TRIANGLES, triangleIndexCount, GL_UNSIGNED_INT, 0);
 
 		//Swap & Poll	
 		glfwSwapBuffers(window);
@@ -57,4 +68,232 @@ void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+}
+
+int init(GLFWwindow*& window)
+{
+	//GLFW Init
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+	//Create window & make active context	
+	window = glfwCreateWindow(1280, 720, "OpenGL_2233", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	//Load GLAD 
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	return 0;
+}
+
+void createTriangle(GLuint& vao, GLuint &ebo, int& size, int &numIndices)
+{
+	/* Makes Square or Triangle based on vertices & indices with color generated in the shader
+	//position				//color
+	float vertices[] = {
+	-0.5f, -0.5f, 0.5f,		1.0f, 0.0f, 0.0f, 1.0f,
+	 0.5f, -0.5f, 0.5f,		0.0f, 1.0f, 0.0f, 1.0f,
+	-0.5f,  0.5f, 0.5f,		0.0f, 0.0f, 1.0f, 1.0f,
+	 0.5f,  0.5f, 0.5f,		1.0f, 1.0f, 1.0f, 1.0f
+	};
+
+	int indices[] = {
+		0,2,1,
+		1,3,2
+	};
+	*/
+
+	// need 24 vertices for normal/uv-mapped Cube
+	float vertices[] = {
+		// positions            //colors            // tex coords   // normals
+		0.5f, -0.5f, -0.5f,     1.0f, 1.0f, 1.0f,   1.f, 0.f,       0.f, -1.f, 0.f,
+		0.5f, -0.5f, 0.5f,      1.0f, 1.0f, 1.0f,   1.f, 1.f,       0.f, -1.f, 0.f,
+		-0.5f, -0.5f, 0.5f,     1.0f, 1.0f, 1.0f,   0.f, 1.f,       0.f, -1.f, 0.f,
+		-0.5f, -0.5f, -.5f,     1.0f, 1.0f, 1.0f,   0.f, 0.f,       0.f, -1.f, 0.f,
+
+		0.5f, 0.5f, -0.5f,      1.0f, 1.0f, 1.0f,   2.f, 0.f,       1.f, 0.f, 0.f,
+		0.5f, 0.5f, 0.5f,       1.0f, 1.0f, 1.0f,   2.f, 1.f,       1.f, 0.f, 0.f,
+
+		0.5f, 0.5f, 0.5f,       1.0f, 1.0f, 1.0f,   1.f, 2.f,       0.f, 0.f, 1.f,
+		-0.5f, 0.5f, 0.5f,      1.0f, 1.0f, 1.0f,   0.f, 2.f,       0.f, 0.f, 1.f,
+
+		-0.5f, 0.5f, 0.5f,      1.0f, 1.0f, 1.0f,   -1.f, 1.f,      -1.f, 0.f, 0.f,
+		-0.5f, 0.5f, -.5f,      1.0f, 1.0f, 1.0f,   -1.f, 0.f,      -1.f, 0.f, 0.f,
+
+		-0.5f, 0.5f, -.5f,      1.0f, 1.0f, 1.0f,   0.f, -1.f,      0.f, 0.f, -1.f,
+		0.5f, 0.5f, -0.5f,      1.0f, 1.0f, 1.0f,   1.f, -1.f,      0.f, 0.f, -1.f,
+
+		-0.5f, 0.5f, -.5f,      1.0f, 1.0f, 1.0f,   3.f, 0.f,       0.f, 1.f, 0.f,
+		-0.5f, 0.5f, 0.5f,      1.0f, 1.0f, 1.0f,   3.f, 1.f,       0.f, 1.f, 0.f,
+
+		0.5f, -0.5f, 0.5f,      1.0f, 1.0f, 1.0f,   1.f, 1.f,       0.f, 0.f, 1.f,
+		-0.5f, -0.5f, 0.5f,     1.0f, 1.0f, 1.0f,   0.f, 1.f,       0.f, 0.f, 1.f,
+
+		-0.5f, -0.5f, 0.5f,     1.0f, 1.0f, 1.0f,   0.f, 1.f,       -1.f, 0.f, 0.f,
+		-0.5f, -0.5f, -.5f,     1.0f, 1.0f, 1.0f,   0.f, 0.f,       -1.f, 0.f, 0.f,
+
+		-0.5f, -0.5f, -.5f,     1.0f, 1.0f, 1.0f,   0.f, 0.f,       0.f, 0.f, -1.f,
+		0.5f, -0.5f, -0.5f,     1.0f, 1.0f, 1.0f,   1.f, 0.f,       0.f, 0.f, -1.f,
+
+		0.5f, -0.5f, -0.5f,     1.0f, 1.0f, 1.0f,   1.f, 0.f,       1.f, 0.f, 0.f,
+		0.5f, -0.5f, 0.5f,      1.0f, 1.0f, 1.0f,   1.f, 1.f,       1.f, 0.f, 0.f,
+
+		0.5f, 0.5f, -0.5f,      1.0f, 1.0f, 1.0f,   2.f, 0.f,       0.f, 1.f, 0.f,
+		0.5f, 0.5f, 0.5f,       1.0f, 1.0f, 1.0f,   2.f, 1.f,       0.f, 1.f, 0.f
+	};
+
+	unsigned int indices[] = {  // note that we start from 0!
+		// DOWN
+		0, 1, 2,   // first triangle
+		0, 2, 3,    // second triangle
+		// BACK
+		14, 6, 7,   // first triangle
+		14, 7, 15,    // second triangle
+		// RIGHT
+		20, 4, 5,   // first triangle
+		20, 5, 21,    // second triangle
+		// LEFT
+		16, 8, 9,   // first triangle
+		16, 9, 17,    // second triangle
+		// FRONT
+		18, 10, 11,   // first triangle
+		18, 11, 19,    // second triangle
+		// UP
+		22, 12, 13,   // first triangle
+		22, 13, 23,    // second triangle
+	};
+
+	//Stride :: Vertices in Amount of columns 
+	int stride = (3 + 3 + 2 + 3) * sizeof(float);
+
+	size = sizeof(vertices);
+	numIndices = sizeof(indices) / sizeof(int);	
+	
+	//Creates VertexArrayObject index
+	glGenVertexArrays(1, &vao);
+	//Bind it to create its configuration
+	glBindVertexArray(vao);
+
+	//VertexBufferObject, bind it and assign vertices to it
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//set layout of vertex data
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, (void*)(3 *  sizeof(float)));
+	glEnableVertexAttribArray(1);
+}
+
+void createShaders()
+{
+	createProgram(simpleProgram, "shaders/simpleVertex.shader", "shaders/simpleFragment.shader");
+}
+
+void createProgram(GLuint& programID, const char* vertex, const char* fragment)
+{
+	//Create a GL Program with a vertex & fragment shader
+	char* vertexSrc;
+	char* fragmentSrc;
+	loadFile(vertex, vertexSrc);
+	loadFile(fragment, fragmentSrc);
+
+	GLuint vertexShaderID;
+	GLuint fragmentShaderID;
+
+	vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShaderID, 1, &vertexSrc, nullptr);
+	glCompileShader(vertexShaderID);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShaderID, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShaderID, 512, nullptr, infoLog);
+		std::cout << "ERROR COMPILING VERTEX SHADER\n" << infoLog << std::endl;
+	}
+
+	fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShaderID, 1, &fragmentSrc, nullptr);
+	glCompileShader(fragmentShaderID);
+
+	glGetShaderiv(fragmentShaderID, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShaderID, 512, nullptr, infoLog);
+		std::cout << "ERROR COMPILING FRAGMENT SHADER\n" << infoLog << std::endl;
+	}
+
+	programID = glCreateProgram();
+	glAttachShader(programID, vertexShaderID);
+	glAttachShader(programID, fragmentShaderID);
+	glLinkProgram(programID);
+
+	glGetProgramiv(programID, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(programID, 512, nullptr, infoLog);
+		std::cout << "ERROR LINKING PROGRAM SHADER\n" << infoLog << std::endl;
+	}
+
+	glDeleteShader(vertexShaderID);
+	glDeleteShader(fragmentShaderID);
+
+	delete vertexSrc;
+	delete fragmentSrc;
+}
+
+void loadFile(const char* filename, char*& output)
+{
+	//open the file
+	std::fstream file(filename, std::ios::in | std::ios::binary); //std::ios::in needed to open the file
+
+	//if the file was succesfully opened
+	if (file.is_open())
+	{
+		//get length of file
+		file.seekg(0, file.end);
+		int length = file.tellg();
+		file.seekg(0, file.beg);
+
+		//allocate memory for the char pointer
+		output = new char[length + 1];
+
+		//read data as a block
+		file.read(output, length);
+
+		//add null terminator to the end of the char pointer
+		output[length] = '\0';
+
+		//close the file
+		file.close();
+	}
+	else
+	{
+		//if the file failed to open, set the char pointer to NULL
+		output = NULL;
+	}
 }
